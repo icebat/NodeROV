d05ab58962c31bcd3012480a7b5e363201843ad7
Toffe87 <thorleif@tjweb.no>
2017-04-23 23:05:07 +0200

    Added gyro calibration on boot

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ ptSensorExt.initialize();
 ptSensorInt.initialize();
 accmag.initialize();
 
+// Calibrate gyro, 10 samples, 10ms appart
+gyro.calibrate(10, 10);
+
 // Create pid controllers used on the script
 rov.depth.PID   = utils.PID(3, 0.0002, 0, 0, -400, 400);
 rov.heading.PID = utils.PID(3, 0.0002, 0, 0, -400, 400);
